94605c322d8cc62390a286f3b808cd43e3ab50bb
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-01-15 02:20:52 -0300

    Classe Payment implementada

--- api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/config/TestConfig.java
+++ api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import src.main.java.com.example.apiprodutosepedidos.entities.Category;
 import src.main.java.com.example.apiprodutosepedidos.entities.Order;
 import src.main.java.com.example.apiprodutosepedidos.entities.OrderItem;
+import src.main.java.com.example.apiprodutosepedidos.entities.Payment;
 import src.main.java.com.example.apiprodutosepedidos.entities.Product;
 import src.main.java.com.example.apiprodutosepedidos.entities.User;
 import src.main.java.com.example.apiprodutosepedidos.entities.enums.OrderStatus;
@@ -82,8 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
-	
-	
 	
 }
