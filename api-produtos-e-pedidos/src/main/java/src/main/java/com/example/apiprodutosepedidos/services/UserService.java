1be5383886200b60d042b0e80a7e60e2e9008bab
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-01-15 05:52:01 -0300

    Tratamento de excecao para o delete do User implementado

--- api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/services/UserService.java
+++ api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import src.main.java.com.example.apiprodutosepedidos.entities.User;
 import src.main.java.com.example.apiprodutosepedidos.repositories.UserRepository;
+import src.main.java.com.example.apiprodutosepedidos.services.exceptions.DatabaseException;
 import src.main.java.com.example.apiprodutosepedidos.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
---
+++ api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package src.main.java.com.example.apiprodutosepedidos.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+	
+}
