52486417cb344eb88484919f3bf30b09201f3d2c
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-01-15 03:21:07 -0300

    Metodo insert de user criado

--- api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/resources/UserResource.java
+++ api-produtos-e-pedidos/src/main/java/src/main/java/com/example/apiprodutosepedidos/resources/UserResource.java
@@ -1,5 +1,6 @@
 package src.main.java.com.example.apiprodutosepedidos.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import src.main.java.com.example.apiprodutosepedidos.entities.User;
 import src.main.java.com.example.apiprodutosepedidos.services.UserService;
@@ -36,6 +38,7 @@ public class UserResource {
 	@PostMapping
 	public ResponseEntity<User> insert(@RequestBody User obj){
 		obj = service.insert(obj);
-		return ResponseEntity.ok().body(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
